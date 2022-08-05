9593ef22aa847fc4082b0a4921a16e4e474a5f92
KRSBX <firdaus.sati51@gmail.com>
2022-08-05 22:01:03 +0700

    chore: init repo with crb

---
+++ src/utils/root.ts
@@ -0,0 +1,12 @@
+import express, { Express } from 'express';
+import cors from 'cors';
+import { queryParserMw } from '../middleware/queryParser';
+
+export default (app: Express) => {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static('public'));
+  app.use(cors());
+
+  app.get('*', queryParserMw);
+}
