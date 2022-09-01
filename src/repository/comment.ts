95dcc21b96add7a831262cdcc41423abb2e2d3eb
KRSBX <firdaus.sati51@gmail.com>
2022-09-01 09:57:34 +0700

    feat: server clusters

--- src/repository/comment.ts
+++ src/repository/comment.ts
@@ -1,5 +1,7 @@
 import _ from 'lodash';
 import { AnyRecord, ModelStructure, MODELS_NAME } from './models';
+import commentLike from './commentLike';
+import commentDislike from './commentDislike';
 import factory from './baseRepository';
 
 const commentRepository = factory(MODELS_NAME.COMMENT);
@@ -14,9 +16,39 @@ const modelToResource = async (model: ModelStructure['comment']) => {
   return model;
 };
 
+const getCommentLikes = async (commentId: number) =>
+  commentLike.count({
+    commentId,
+  });
+
+const getCommentDislikes = async (commentId: number) =>
+  commentDislike.count({
+    commentId,
+  });
+
+const getCommentLikeStatus = async (commentId: number, userId: number) =>
+  !_.isEmpty(
+    await commentLike.findOne({
+      commentId,
+      userId,
+    })
+  );
+
+const getCommentDislikeStatus = async (commentId: number, userId: number) =>
+  !_.isEmpty(
+    await commentDislike.findOne({
+      commentId,
+      userId,
+    })
+  );
+
 const extendCommentRepository = {
   modelToResource,
   resourceToModel,
+  getCommentLikes,
+  getCommentDislikes,
+  getCommentLikeStatus,
+  getCommentDislikeStatus,
 };
 
 const repository = _.merge(commentRepository, extendCommentRepository);
--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -2,3 +2,8 @@ export const USER_ROLE = {
   ADMIN: 'ADMIN',
   USER: 'USER',
 } as const;
+
+export const SERVER_MODE = {
+  DEVELOPMENT: 'DEVELOPMENT',
+  PRODUCTION: 'PRODUCTION',
+} as const;
